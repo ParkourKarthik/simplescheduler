3895e236e3c28e3025cccf455b787921b702b419
parkourkarthik <parkourkarthik@gmail.com>
2020-02-19 11:20:13 +0530

    heroku api integration

--- ui/src/config.js
+++ ui/src/config.js
@@ -1,5 +1,8 @@
 const config = {
-  apiUrl: 'http://localhost:8080'
-}
+  apiUrl:
+    process.env.NODE_ENV == "dev"
+      ? "http://localhost:8080"
+      : "https://pk-simplescheduler.herokuapp.com/"
+};
 
 export default config;
